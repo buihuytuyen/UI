87964a141a2b7d80c36083d878a66c5f43d7162f
Bùi Huy Tuyền <buihuytuyen9912@gmail.com>
2022-09-15 22:07:43 +0700

    Thư viện UI

---
+++ script.js
@@ -0,0 +1,17 @@
+// import html from './core.js'
+
+// const cars = ['BMV', 'Porsche', 'Mercedes']
+
+// const output = html`
+//     <h1>${false}</h1>
+//     <ul>
+//         ${cars.map(car => `<li>${car}</li>`)}
+//     </ul>
+// `
+
+// console.log(output)
+
+import { attach} from './store.js'
+import App from './component/App.js'
+
+attach(App, document.getElementById('root'))
